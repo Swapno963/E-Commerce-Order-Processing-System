e9f96d97a16af16337cea75d687c3184767427d9
swapno963 <swapno963@gmail.com>
2026-05-02 19:57:06 +0600

    add initial data classes for User, Product, Order, and OrderItem

---
+++ main.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    id: int
+    name: str
+
+@dataclass
+class Product:
+    id: int
+    name: str
+    price: float
+    stock: int
+
+
+@dataclass
+class Order:
+    id: int
+    order_item: int
+    user_id: int
+
+@dataclass
+class OrderItem:
+    id: int
+    product_id: int
+    quantity: int
+
+
