916998052f4adb87b2b51239641becaf8f24cc83
swapno963 <swapno963@gmail.com>
2026-05-02 20:13:58 +0600

    add entity classes for Order, OrderItem, Product, and User

---
+++ entities/  order.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Order:
+    id: int
+    order_item: int
+    user_id: int
+
+
+
+
---
+++ entities/user.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    id: int
+    name: str
+    email: str
+    password: str
+
+
+
