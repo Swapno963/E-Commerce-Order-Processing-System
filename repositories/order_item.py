603c5db28b26d7a2a2e06c1fbbcfb8c34c9e013d
swapno963 <swapno963@gmail.com>
2026-05-02 20:08:54 +0600

    add OrderItemRepository class for managing order item storage

---
+++ repositories/order_item.py
@@ -0,0 +1,11 @@
+from main import OrderItem
+
+class OrderItemRepository:
+    def __init__(self):
+        self.storage = {}
+
+    def save(self, order_item):
+        self.storage[order_item.id] = order_item
+
+    def get_by_id(self, item_id):
+        return self.storage.get(item_id)
