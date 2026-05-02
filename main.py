80556261abe4066e02625e16a553eb7bb151d10b
swapno963 <swapno963@gmail.com>
2026-05-02 21:45:37 +0600

    implement main menu and login functionality with AuthService integration

--- main.py
+++ main.py
@@ -1,2 +1,61 @@
+from repositories.user import UserRepository, AuthService
+
+user_id = 0
+is_logged_in = False
+repo = UserRepository()
+
+auth_service = AuthService(repo)
 
 
+while True:
+    if is_logged_in:
+        print("\n\n\n===== MENU =====")
+        print("1. View Products")
+        print("2. Create Order")
+        print("3. View Orders")
+        print("4. Exit")
+
+    else:
+        print("\n\n\n===== MENU =====")
+        print("a. Login")
+        print("b. Registration")
+        print("c. Demo login")
+        print("d. Exit")
+
+
+    choice = input("Enter choice: ")
+
+
+    if choice =="1":
+        print("One chosen")
+    elif choice == "d":
+        print("Exiting...")
+        break
+    if choice == "a":
+        email = input("Enter Email: ")
+        password = input("Enter Password: ")
+        message, status = auth_service.login(
+            email=email,
+            password=password
+        )
+
+        # email="swapno@gmail.com",
+            # password="1234"
+        if status == 200:
+            is_logged_in = True
+            print(message)
+        else:
+            print("Login failed!")
+
+    if choice == "c":
+        message, status = auth_service.login(
+            email="swapno@gmail.com",
+            password="1234"
+        )
+        if status == 200:
+            is_logged_in = True
+            print(message)
+        else:
+            print("Login failed!")
+    else:
+        print("Invalid choice")
