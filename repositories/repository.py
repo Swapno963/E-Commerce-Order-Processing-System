893e393cc601f3d800573e2ef210a07db23926c8
swapno963 <swapno963@gmail.com>
2026-05-02 20:04:42 +0600

    add UserRepository and AuthService classes with user authentication methods

---
+++ repositories/repository.py
@@ -0,0 +1,63 @@
+from main import User
+
+class UserRepository:
+    def __init__(self):
+        self.storage = {
+            1: User(
+                id=1,
+                name="Swapno",
+                email="swapno@gmail.com",
+                password="1234"
+            ),
+            2: User(
+                id=2,
+                name="Alex",
+                email="alex@gmail.com",
+                password="abcd"
+            )
+        }
+
+    def get_by_id(self, user_id):
+        return self.storage.get(user_id)
+
+    def save(self, user):
+        self.storage[user.id] = user
+
+    def get_user_by_email(self, email):
+        for user in self.storage.values():
+            if user.email == email:
+                return user
+
+        return None
+
+
+class AuthService:
+    def __init__(self, user_repo):
+        self.user_repo = user_repo
+
+    def login(self, email, password):
+        user = self.user_repo.get_user_by_email(email)
+
+        if not user:
+            raise Exception("User not found")
+
+        if user.password != password:
+            raise Exception("Invalid password")
+
+        return f"Welcome {user.name}"
+
+
+# =========================
+# USAGE
+# =========================
+
+repo = UserRepository()
+
+auth_service = AuthService(repo)
+
+result = auth_service.login(
+    email="swapno@gmail.com",
+    password="1234"
+)
+
+print(result)
